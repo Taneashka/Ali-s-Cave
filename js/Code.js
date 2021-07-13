253767ed9c4caf60d0d868ebc0567e1f8852490b
Taneashka <76481711+Taneashka@users.noreply.github.com>
2021-07-13 10:22:21 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("pink")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 600,150)
+    fill("pink")
+    text("Hint: An organized collection of structured information !!", 600,170)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 100,290)
+    fill("pink")
+    text("Hint: It is created only once and is called throught the code !!", 100, 310)
+    
+
+}
